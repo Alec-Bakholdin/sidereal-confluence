861c8078027d1d652ec2e9bdbe3abb38076ecaea
Alec Bakholdin <alecbakholdin@gmail.com>
2022-04-21 19:34:00 -0400

    Initial Commit

---
+++ src/main/java/com/bakholdin/siderealconfluence/SiderealConfluenceApplication.java
@@ -0,0 +1,13 @@
+package com.bakholdin.siderealconfluence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiderealConfluenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiderealConfluenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bakholdin/siderealconfluence/SiderealConfluenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakholdin.siderealconfluence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiderealConfluenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
