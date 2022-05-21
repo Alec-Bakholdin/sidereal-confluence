d631abc60c8c58b01092019ddcba7503100e6d27
Alec Bakholdin <alecbakholdin@gmail.com>
2022-05-21 17:49:37 -0400

    Exception Handling works now!

--- src/main/java/com/bakholdin/siderealconfluence/controllers/advice/SocketControllerAdvice.java
+++ src/main/java/com/bakholdin/siderealconfluence/controllers/advice/SocketControllerAdvice.java
@@ -2,29 +2,35 @@ package com.bakholdin.siderealconfluence.controllers.advice;
 
 import com.bakholdin.siderealconfluence.controllers.socket.OutgoingSocketTopics;
 import com.bakholdin.siderealconfluence.dto.ErrorDto;
-import com.bakholdin.siderealconfluence.dto.UserDto;
+import com.bakholdin.siderealconfluence.entity.User;
+import com.bakholdin.siderealconfluence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+
+import java.security.Principal;
 
 @ControllerAdvice
 @RequiredArgsConstructor
-public class SocketControllerAdvice {
+public class SocketControllerAdvice extends DefaultHandlerExceptionResolver {
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final UserRepository userRepository;
 
     @MessageExceptionHandler(RuntimeException.class)
-    public void handleException(RuntimeException e) {
-        //UserDto userDto = SocketUtil.getUserDto(principal);
-        UserDto userDto = UserDto.builder().build();
+    public void handleException(RuntimeException e, @AuthenticationPrincipal Principal principal) {
+        User user = userRepository.findByUsername(principal.getName())
+                .orElseThrow(() -> new RuntimeException("User not found"));
         ErrorDto errorDto = ErrorDto.builder()
                 .message(e.getMessage())
                 .build();
-        if (userDto.getGame() != null) {
-            String errorTopic = OutgoingSocketTopics.TOPIC_GAME_ERRORS(userDto.getGame().getId());
+        if (user.getGame() != null) {
+            String errorTopic = OutgoingSocketTopics.TOPIC_GAME_ERRORS(user.getGame().getId());
             simpMessagingTemplate.convertAndSend(errorTopic, errorDto);
         } else {
-            simpMessagingTemplate.convertAndSendToUser(userDto.getName(), OutgoingSocketTopics.USER_ERRORS, errorDto);
+            simpMessagingTemplate.convertAndSendToUser(principal.getName(), OutgoingSocketTopics.USER_ERRORS, errorDto);
         }
     }
 }
